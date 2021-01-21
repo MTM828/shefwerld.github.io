88c3b6eb568cddaaa6c213dace125558a02c9879
MTM828 <71281115+MTM828@users.noreply.github.com>
2021-01-21 21:57:50 +0300

    Update e.js

--- game/e.js
+++ game/e.js
@@ -1,7 +1,12 @@
+const errMsg = document.querySelector('#errorDisplay');
+
 const canvas = document.querySelector('#canvas');
 const ctx = canvas.getContext('2d');
+
 function mainLoop() {
   ctx.fillStyle = 'rgb(135, 206, 235)';
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
-setInterval(mainLoop, 20)
+setInterval(
+  function(){try{mainLoop();}catch(err){errMsg.innerHTML = err;}}
+, 20)
