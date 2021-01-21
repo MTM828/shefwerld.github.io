7adff34f8991195a2af63e64b2c74de32f4dafca
MTM828 <71281115+MTM828@users.noreply.github.com>
2021-01-21 17:43:39 +0300

    Update e.js

--- game/e.js
+++ game/e.js
@@ -1,6 +1,7 @@
 const canvas = document.querySelector('#canvas');
 const ctx = canvas.getContext('2d');
 function mainLoop() {
-  
+  ctx.fillStyle = 'rgb(135, 206, 235)';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
 setInterval(mainLoop, 20)
