6c8e664cbeab89506ab55786b9e46fe061cf35bb
MTM828 <71281115+MTM828@users.noreply.github.com>
2021-01-22 20:32:51 +0300

    Update e.js

--- game/e.js
+++ game/e.js
@@ -9,12 +9,28 @@ const ctx = canvas.getContext('2d');
 
 var i;
 var j;
+function col(x1, y1, w1, h1, x2, y2, w2, h2) {
+  return ((x1-w1*0.5 < x2-w2*-0.5) && (x1+w1*0.5 > x2+w2*-0.5)) && ((y1-h1*0.5 < y2-h2*-0.5) && (y1+h1*0.5 > y2+h2*-0.5));
+}
 var player = {
   x: canvas.width  / 2,
   y: canvas.height / 2,
   velX:  0,
   velY:  0,
   frame: 0,
+  col: function() {
+    for (i = 0; i < platforms.length; i++) {
+      if (col(this.x, this.y, 40, 40, platforms[i].x, platforms[i].y, platforms[i].length, platforms[i].height)) {return true;}
+    }
+    return false;
+  },
+}
+var physics = {
+  jumpHeight: 12,
+  maxGravity: 25,
+  movementSpeed: 0.3,
+  friction: 0.1,
+  maxMovementSpeed: 5,
 }
 var platforms = [
   {type: 'ground',   x: canvas.width / 2,      y: canvas.height / 2 + 50, width: 50, height: 50},
@@ -22,7 +38,17 @@ var platforms = [
 ]
 
 function mainLoop() {
-  function update() {}
+  function update() {
+    player.velY += physics.gravity;
+    if (player.col) {
+      while (player.col) {
+        player.y -= 0.1;
+      }
+      player.velY = 0;
+    }
+    player.y += player.velY;
+    player.x += player.velX;
+  }
   function clear(clr) {
     ctx.save();
     ctx.fillStyle = clr;
