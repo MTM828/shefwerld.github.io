8162c34103826a5ff7b319b8091fb3f5d3b3aae6
MTM828 <71281115+MTM828@users.noreply.github.com>
2021-03-14 00:21:29 +0300

    Update game.js

--- game/dreemlend/game.js
+++ game/dreemlend/game.js
@@ -1,33 +1,29 @@
 var begin = true;
 while (!begin) {}
 
-const errMsg = document.querySelector('#errorDisplay');
 window.onload = function() {
-    try {
-        mainLoop();
-    } catch (err) {
-        errMsg.innerHTML = err.stack;
-    }
-}
+    setTimeout(mainLoop, 0);
+};
 const canvas = document.querySelector('#canvas');
 const ctx = canvas.getContext('2d');
 
 var i;
 var j;
+var k;
 
 var currentTime;
 var elapsedTime;
 var oldTime;
 var frameCount = 0;
 var fps = 60;
-var frameDelay = 1000 / fps;
+var frameDelay = 1000/fps;
 
 function col(x1, y1, w1, h1, x2, y2, w2, h2) {
     return ((x1 + w1 > x2) && (x1 < x2 + w2)) && ((y1 + h1 > y2) && (y1 < y2 + h2));
 }
 function colliding() {
     var returnVal = false;
-    for (i = 0; i < platforms.length; i++) {
+    for (i=0; i<platforms.length; i++) {
         if (col(
                 player.x - 20,
                 player.y + 20,
@@ -41,9 +37,24 @@ function colliding() {
     }
     return returnVal;
 }
+function enmCol(index) {
+	for (j=0; j<platforms.length; j++) {
+		if (col(
+		    -enemies[index].x,
+			enemies[index].y,
+			35,
+			35,
+			platforms[j].x,
+			platforms[j].y,
+			platforms[j].width,
+			platforms[j].height
+		)) {return true;}
+	}
+	return false;
+}
 
 const spriteSheet = new Image();
-spriteSheet.src = './spritesheet.png';
+spriteSheet.src = './SpriteSheet.png';
 
 var player = {
     x: 0,
@@ -51,7 +62,9 @@ var player = {
     velX: 0,
     velY: 0,
     frame: 0,
-}
+	reload: 0,
+	reloadPeriod: 75
+};
 var physics = {
     jumpHeight: 12,
     gravity: 0.3,
@@ -59,51 +72,24 @@ var physics = {
     movementSpeed: 0.3,
     friction: 0.9,
     maxMovementSpeed: 5,
-}
+};
 var platforms = [
+    {type: 'ground',   x: 0,   y: 100, width: 500,  height: 50},
+    {type: 'ground',   x: 0,   y: 300, width: 500,  height: 50},
+    {type: 'obstacle', x: 0,   y: 500, width: 500,  height: 50},
+    {type: 'ground',   x: 250, y: 700, width: 1000, height: 50},
+    {type: 'ground',   x: 450, y: 500, width: 50,   height: 50},
+    {type: 'ground',   x: 250, y: 300, width: 50,   height: 50}
+];
+var projectiles = [];
+var enemies = [
     {
-        type: 'ground',
-        x: 0,
-        y: 100,
-        width: 500,
-        height: 50
-    },
-    {
-        type: 'ground',
-        x: 0,
-        y: 300,
-        width: 500,
-        height: 50
-    },
-    {
-        type: 'obstacle',
-        x: 0,
-        y: 500,
-        width: 500,
-        height: 50
-    },
-    {
-        type: 'ground',
-        x: 250,
-        y: 700,
-        width: 1000,
-        height: 50
-    },
-    {
-        type: 'ground',
-        x: 450,
-        y: 500,
-        width: 50,
-        height: 50
-    },
-    {
-        type: 'ground',
-        x: 250,
-        y: 300,
-        width: 50,
-        height: 50
-    },
-]
+	    x: -400,
+		y: 0,
+		dir: 1,
+		type: 1
+	}
+];
 
 var keys = {
     w: false,
@@ -115,12 +101,12 @@ var keys = {
     upArrow: false,
     dnArrow: false,
     space: false
-}
+};
 var mouse = {
     down: false,
     x: 0,
     y: 0
-}
+};
 function onKeyDown(event) {
     var keyCode = event.which;
     if (keyCode == 87) {
@@ -185,27 +171,18 @@ function onMouseMove(event) {
     mouse.x = event.offsetX;
     mouse.y = event.offsetY;
 }
-function onMouseDown(event) {
-    mouse.down = true;
-}
-function onMouseUp(event) {
-    mouse.down = false;
-}
 document.addEventListener("keydown", onKeyDown, false);
 document.addEventListener("keyup", onKeyUp, false);
 document.addEventListener("mousemove", onMouseMove, false);
-document.addEventListener("mousedown", onMouseDown, false);
-document.addEventListener("mouseup", onMouseUp, false);
+document.addEventListener("mouseup", function(){mouse.down=false;}, false);
+document.addEventListener("mousedown", function(){mouse.down=true;}, false);
 
 oldTime = window.performance.now();
-
 function mainLoop() {
     currentTime = window.performance.now();
     elapsedTime = currentTime - oldTime;
     requestAnimationFrame(mainLoop);
-    if (!elapsedTime > frameDelay) {
-        while (!elapsedTime > frameDisplay) {}
-    }
+    while (!elapsedTime > frameDelay) {}
     oldTime = currentTime;
 
     function update() {
@@ -259,10 +236,33 @@ function mainLoop() {
             }
             player.velX = 0;
         }
-        player.frame += 1;
-        if (player.frame > 2) {
-            player.frame = 0;
-        }
+
+		player.frame += 1;
+        // if (player.frame > 5 - 1) {player.frame = 0;}
+
+        if (player.reload < player.reloadPeriod) {player.reload += 1;}
+        if (mouse.down && player.reload >= player.reloadPeriod) {
+			player.reload = 0;
+			projectiles.push({
+				x: player.x - 20 + canvas.width/2,
+				y: player.y - 20 + canvas.height/2,
+				vel: 10,
+				dir: Math.atan2(mouse.y - canvas.height/2, mouse.x - canvas.width/2)
+			});
+		}
+		for (i=0; i<projectiles.length; i++) {
+			projectiles[i].x += Math.cos(projectiles[i].dir) * projectiles[i].vel;
+			projectiles[i].y += Math.sin(projectiles[i].dir) * projectiles[i].vel;
+		}
+
+        for (i=0; i<enemies.length; i++) {
+			while (!enmCol(i)) {enemies[i].y += 1;}
+
+			enemies[i].x += enemies[i].dir;
+			if (!enmCol(i)) {enemies[i].x -= 1; enemies[i].dir *= -1}
+
+			if (enmCol(i)) {enemies[i].y -= 1;}
+		}
     }
 
     function clear(clr) {
@@ -272,9 +272,8 @@ function mainLoop() {
         ctx.restore();
     }
     function render() {
-        ctx.save();
         clear('rgb(135, 206, 235)');
-        for (i = 0; i < platforms.length; i++) {
+        for (i=0; i<platforms.length; i++) {
             switch (platforms[i].type) {
                 case 'ground':
                     ctx.fillStyle = 'rgb(0, 179, 44)';
@@ -284,12 +283,55 @@ function mainLoop() {
                     break;
             }
             ctx.fillRect(
-                platforms[i].x + 30 - platforms[i].width + canvas.width / 2 - player.x,
-                platforms[i].y - 20 - platforms[i].height + canvas.height / 2 - player.y,
+                platforms[i].x + 30 - platforms[i].width  + canvas.width/2  - player.x,
+                platforms[i].y - 20 - platforms[i].height + canvas.height/2 - player.y,
                 platforms[i].width,
                 platforms[i].height
             );
         }
+		for (i=0; i<enemies.length; i++) {
+			switch (enemies[i].type) {
+				case 1:
+                    ctx.drawImage(
+	        	        spriteSheet,
+        		    	0,
+    			        40,
+		        	    40,
+	    	        	40,
+    		        	enemies[i].x - player.x - 15 + canvas.width/2,
+		            	enemies[i].y - player.y - 70 + canvas.height/2,
+	    		        35,
+    			        35
+		            );
+					break;
+				case 2:
+                    ctx.drawImage(
+	        	        spriteSheet,
+        		    	0,
+    			        80,
+		        	    40,
+	    	        	40,
+    		        	enemies[i].x - player.x - 15 + canvas.width/2,
+		            	enemies[i].y - player.y - 70 + canvas.height/2,
+	    		        35,
+    			        35
+		            );
+					break;
+			}
+		}
+		for (i=0; i < projectiles.length; i++) {
+		   	ctx.drawImage(
+	    	    spriteSheet,
+    			0,
+			    120,
+		    	40,
+	    		40,
+    			projectiles[i].x - player.x,
+		    	projectiles[i].y - player.y,
+			    40,
+			    40
+		    );
+		}
         ctx.drawImage(
             spriteSheet,
             player.frame * 40,
@@ -301,21 +343,9 @@ function mainLoop() {
             35,
             35
         );
-		if (mouse.down) {
-		   	ctx.drawImage(
-	    	    spriteSheet,
-    			0,
-			    40,
-		    	40,
-	    		40,
-    			mouse.x,
-		    	mouse.y,
-			    40,
-			    40
-		    );
-		}
-        ctx.restore();
     }
+
     update();
     render();
+	frameCount += 1;
 }
